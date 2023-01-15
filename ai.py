5d55ec4907262e530af1acd5ac94cdf3f1396816
SuleymanKeles <61537773+SuleymanKeles@users.noreply.github.com>
2023-01-15 19:26:31 +0300

    bug fix
    menu options - ai class bug fix

--- ai.py
+++ ai.py
@@ -15,6 +15,7 @@ def swap_player(player):
 
 class Ai:
 
+<<<<<<< Updated upstream
     def __init__(self, depthLimit, heuristic):
         self.depthLimit = depthLimit
         self.heuristic = None
@@ -23,7 +24,24 @@ class Ai:
         elif heuristic == "2":
             self.heuristic = self.heuristic2
         elif heuristic == "3":
+=======
+    def choose_heuristic(self):
+        if self.heuristic_choose == '1':
+            self.heuristic = self.heuristic1
+        elif self.heuristic_choose == '2':
+            self.heuristic = self.heuristic2
+        elif self.heuristic_choose == '3':
+>>>>>>> Stashed changes
             self.heuristic = self.heuristic3
+        else:
+            print("ERROR: Your heuristic choose is wrong or have a lot of number.")
+
+    def __init__(self, heuristic_choose):
+        self.heuristic_choose = heuristic_choose
+        print("hello i am ai.")
+        self.depthLimit = 0
+        self.heuristic = None
+        self.choose_heuristic()
 
     def ai_move(self, board):
         score, move = self.alpha_beta(board, self.depthLimit, 'X', -math.inf, math.inf)
@@ -37,22 +55,22 @@ class Ai:
             return self.heuristic(board, player), -1
 
         if player == 'X':
-            bestScore = -math.inf
+            best_score = -math.inf
             swap = swap_alpha
         else:
-            bestScore = math.inf
+            best_score = math.inf
             swap = swap_beta
 
-        bestMove = -2
+        best_move = -2
 
         children = board.make_children(player)
 
         for child in children:
             move, childboard = child
             temp = self.alpha_beta(childboard, depth - 1, swap_player(player), alpha, beta)[0]
-            if swap(temp, bestScore):
-                bestScore = temp
-                bestMove = move
+            if swap(temp, best_score):
+                best_score = temp
+                best_move = move
 
             if player == 'X':
                 alpha = max(alpha, temp)
@@ -60,33 +78,51 @@ class Ai:
                 beta = min(beta, temp)
             if alpha >= beta:
                 break
-        return bestScore, bestMove
+        return best_score, best_move
 
-    def heuristic1(self, board, player):
+    @staticmethod
+    def heuristic1(board, player):
         score = 0
         state = board.board
         # check rows
         for row in range(7):
             for col in range(4):
+<<<<<<< Updated upstream
                 if state[row][col] == player and state[row][col + 1] == player and state[row][col + 2] == player and state[row][col + 3] == player:
+=======
+                if state[row][col] == player and state[row][col + 1] == player and state[row][col + 2] == player and \
+                        state[row][col + 3] == player:
+>>>>>>> Stashed changes
                     score += 1
         # check columns
         for col in range(7):
             for row in range(3):
-                if state[row][col] == player and state[row + 1][col] == player and state[row + 2][col] == player and state[row + 3][col] == player:
+                if state[row][col] == player and state[row + 1][col] == player and state[row + 2][col] == player and \
+                        state[row + 3][col] == player:
                     score += 1
         # check diagonals
         for row in range(3):
             for col in range(4):
+<<<<<<< Updated upstream
                 if state[row][col] == player and state[row + 1][col + 1] == player and state[row + 2][col + 2] == player and state[row + 3][col + 3] == player:
                     score += 1
         for row in range(3):
             for col in range(3, 7):
                 if state[row][col] == player and state[row + 1][col - 1] == player and state[row + 2][col - 2] == player and state[row + 3][col - 3] == player:
+=======
+                if state[row][col] == player and state[row + 1][col + 1] == player and state[row + 2][
+                    col + 2] == player and state[row + 3][col + 3] == player:
+                    score += 1
+        for row in range(3):
+            for col in range(3, 7):
+                if state[row][col] == player and state[row + 1][col - 1] == player and state[row + 2][
+                    col - 2] == player and state[row + 3][col - 3] == player:
+>>>>>>> Stashed changes
                     score += 1
         return score
 
-    def heuristic2(self, board, player):
+    @staticmethod
+    def heuristic2(board, player):
         score = 0
         opponent = 'X' if player == 'O' else 'O'
         state = board.board
@@ -94,36 +130,44 @@ class Ai:
             for col in range(board.WIDTH):
                 if state[row][col] == player:
                     if row < 3:
-                        if state[row + 1][col] == player and state[row + 2][col] == player and state[row + 3][col] == player:
+                        if state[row + 1][col] == player and state[row + 2][col] == player and state[row + 3][
+                            col] == player:
                             score += 1
                     if col < 4:
-                        if state[row][col + 1] == player and state[row][col + 2] == player and state[row][col + 3] == player:
+                        if state[row][col + 1] == player and state[row][col + 2] == player and state[row][
+                            col + 3] == player:
                             score += 1
                     if row < 3 and col < 4:
-                        if state[row + 1][col + 1] == player and state[row + 2][col + 2] == player and state[row + 3][col + 3] == player:
+                        if state[row + 1][col + 1] == player and state[row + 2][col + 2] == player and state[row + 3][
+                            col + 3] == player:
                             score += 1
                     if row < 3 and col > 2:
-                        if state[row + 1][col - 1] == player and state[row + 2][col - 2] == player and state[row + 3][col - 3] == player:
+                        if state[row + 1][col - 1] == player and state[row + 2][col - 2] == player and state[row + 3][
+                            col - 3] == player:
                             score += 1
                         for row in range(board.HEIGHT):
                             for col in range(board.WIDTH):
                                 if state[row][col] == opponent:
                                     if row < 3:
-                                        if state[row + 1][col] == opponent and state[row + 2][col] == opponent and state[row + 3][col] == opponent:
+                                        if state[row + 1][col] == opponent and state[row + 2][col] == opponent and \
+                                                state[row + 3][col] == opponent:
                                             score -= 1
                                     if col < 4:
-                                        if state[row][col + 1] == opponent and state[row][col + 2] == opponent and state[row][col + 3] == opponent:
+                                        if state[row][col + 1] == opponent and state[row][col + 2] == opponent and \
+                                                state[row][col + 3] == opponent:
                                             score -= 1
                                     if row < 3 and col < 4:
-                                        if state[row + 1][col + 1] == opponent and state[row + 2][col + 2] == opponent and state[row + 3][col + 3] == opponent:
+                                        if state[row + 1][col + 1] == opponent and state[row + 2][
+                                            col + 2] == opponent and state[row + 3][col + 3] == opponent:
                                             score -= 1
                                     if row < 3 and col > 2:
-                                        if state[row + 1][col - 1] == opponent and state[row + 2][col - 2] == opponent and state[row + 3][col - 3] == opponent:
+                                        if state[row + 1][col - 1] == opponent and state[row + 2][
+                                            col - 2] == opponent and state[row + 3][col - 3] == opponent:
                                             score -= 1
         return score
 
-
-    def heuristic3(self, board, player):
+    @staticmethod
+    def heuristic3(board, player):
         player_1 = 'X'
         player_2 = 'O'
         score = 0
@@ -175,17 +219,21 @@ class Ai:
                     # add player one streaks to score
                     if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == player_1:
                         score += 100
-                    if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == state[i + 2][j + 2] == player_1:
+                    if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == state[i + 2][
+                        j + 2] == player_1:
                         score += 100
-                    if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == state[i + 2][j + 2] == state[i + 3][j + 3] == player_1:
+                    if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == state[i + 2][j + 2] == \
+                            state[i + 3][j + 3] == player_1:
                         score += 10000
 
                     # add player two streaks to score
                     if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == player_2:
                         score -= 100
-                    if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == state[i + 2][j + 2] == player_2:
+                    if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == state[i + 2][
+                        j + 2] == player_2:
                         score -= 100
-                    if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == state[i + 2][j + 2] == state[i + 3][j + 3] == player_2:
+                    if not j + 3 > board.HEIGHT and state[i][j] == state[i + 1][j + 1] == state[i + 2][j + 2] == \
+                            state[i + 3][j + 3] == player_2:
                         score -= 10000
                 except IndexError:
                     pass
@@ -197,7 +245,8 @@ class Ai:
                         score += 10
                     if not j - 3 < 0 and state[i][j] == state[i + 1][j - 1] == state[i + 2][j - 2] == player_1:
                         score += 100
-                    if not j - 3 < 0 and state[i][j] == state[i + 1][j - 1] == state[i + 2][j - 2] == state[i + 3][j - 3] == player_1:
+                    if not j - 3 < 0 and state[i][j] == state[i + 1][j - 1] == state[i + 2][j - 2] == state[i + 3][
+                        j - 3] == player_1:
                         score += 10000
 
                     # subtract player two streaks
@@ -205,7 +254,8 @@ class Ai:
                         score -= 10
                     if not j - 3 < 0 and state[i][j] == state[i + 1][j - 1] == state[i + 2][j - 2] == player_2:
                         score -= 100
-                    if not j - 3 < 0 and state[i][j] == state[i + 1][j - 1] == state[i + 2][j - 2] == state[i + 3][j - 3] == player_2:
+                    if not j - 3 < 0 and state[i][j] == state[i + 1][j - 1] == state[i + 2][j - 2] == state[i + 3][
+                        j - 3] == player_2:
                         score -= 10000
                 except IndexError:
                     pass
--- game.py
+++ game.py
@@ -1,5 +1,6 @@
 from ai import Ai
 from board import Board
+import time
 
 
 class Game:
@@ -42,10 +43,15 @@ class Game:
             move = self.input_player(self.player_2)
             self.board.make_move_manuel_player(self.player_2, move)
 
-    def human_vs_ai(self, heuristic):
+    def human_vs_ai(self, heuristic, depth):
         print("Starting a game of Human vs AI...")
         # Code to start the game goes here
+<<<<<<< Updated upstream
         ai = Ai(6, heuristic)
+=======
+        ai = Ai(heuristic)
+        ai.depthLimit = int(depth)
+>>>>>>> Stashed changes
         while True:
 
             self.board.print_board()
@@ -59,29 +65,55 @@ class Game:
             result = self.board.check_board(self.player_1, self.player_2)
             if result != 0:
                 break
+            start_time = time.time()
             move = ai.ai_move(self.board)
-            print("move "+ str(move))
+            end_time = time.time()
+            run_time = end_time - start_time
+            print("Run time: AI player 1", run_time)
+            print("move " + str(move))
             self.board.make_move_AI(self.player_2, move)
 
-    def ai_vs_ai(self, heuristic_1, heuristic_2):
+    def ai_vs_ai(self, heuristic_1, heuristic_2, depth1, depth2):
         print("Starting a game of AI vs AI...")
         # Code to start the game goes here
+<<<<<<< Updated upstream
         ai_1 = Ai(6, heuristic_1)
         ai_2 = Ai(6, heuristic_2)
+=======
+        ai_1 = Ai(heuristic_1)
+        ai_2 = Ai(heuristic_2)
+        ai_1.depthLimit = int(depth1)
+        ai_2.depthLimit = int(depth2)
+>>>>>>> Stashed changes
         while True:
+
+
+
             self.board.print_board()
             result = self.board.check_board(self.player_1, self.player_2)
             if result != 0:
                 break
+
+            start_time = time.time()
             move = ai_1.ai_move(self.board)
+            end_time = time.time()
+            run_time = end_time - start_time
+            print("Run time: AI player 1", run_time)
+
             self.board.make_move_AI(self.player_1, move)
 
             self.board.print_board()
             result = self.board.check_board(self.player_1, self.player_2)
             if result != 0:
                 break
+
+            start_time = time.time()
             move = ai_2.ai_move(self.board)
-            print("move "+ str(move))
+            end_time = time.time()
+            run_time = end_time - start_time
+            print("Run time: AI player 2", run_time)
+
+            print("move " + str(move))
             self.board.make_move_AI(self.player_2, move)
 
     def play_game(self):
@@ -96,24 +128,39 @@ class Game:
         if choice == "1":
             self.human_vs_human()
         elif choice == "2":
-            print("1. h1 is basic")
+            print("\n1. h1 is basic")
             print("2. h2 is medium")
             print("3. h3 is hard")
             print("Choose a heuristic :")
             heuristic = input("Please select an option: ")
-            self.human_vs_ai(heuristic)
+            if heuristic != '1' and heuristic != '2' and heuristic != '3':
+                print("Invalid choice. Please select a valid option.")
+                self.main_menu()
+            print("\nChoose a depth level for AI Player ")
+            depth = input("depth level :")
+            self.human_vs_ai(heuristic, depth)
         elif choice == "3":
-            print("1. h1 is basic")
+            print("\n1. h1 is basic")
             print("2. h2 is medium")
             print("3. h3 is hard")
             print("Choose a heuristic for AI Player 1")
             heuristic1 = input("Please select an option: ")
-            print("1. h1 is basic")
+            if heuristic1 != '1' and heuristic1 != '2' and heuristic1 != '3':
+                print("Invalid choice. Please select a valid option.")
+                self.main_menu()
+            print("\n1. h1 is basic")
             print("2. h2 is medium")
             print("3. h3 is hard")
             print("Choose a heuristic for AI Player 2")
             heuristic2 = input("Please select an option: ")
-            self.ai_vs_ai(heuristic1, heuristic2)
+            if heuristic2 != '1' and heuristic2 != '2' and heuristic2 != '3':
+                print("Invalid choice. Please select a valid option.")
+                self.main_menu()
+            print("\nChoose a depth level for AI Player 1")
+            depth1 = input("depth level :")
+            print("\nChoose a depth level for AI Player 2")
+            depth2 = input("depth level :")
+            self.ai_vs_ai(heuristic1, heuristic2, depth1, depth2)
         else:
             print("Invalid choice. Please select a valid option.")
             self.main_menu()
